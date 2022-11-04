c5be2b0291f45ff4982de9551010cf222e5cd693
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2022-11-04 18:04:32 +0330

    upadte RoomAdmin

--- rooms/admin.py
+++ rooms/admin.py
@@ -5,13 +5,77 @@ from . import models
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
   '''Room Admin Definition '''
-  pass
+
+  fieldsets = [('Basic Info', {
+      'fields': [
+          'name',
+          'description',
+          'country',
+          'address',
+          'price',
+      ]
+  }), ('Times', {
+      'fields': ['check_in', 'check_out', 'instant_book']
+  }),
+               ('More About the Space', {
+                   'classes': ['collapse'],
+                   'fields': [
+                       'amenities',
+                       'facilities',
+                       'house_rule',
+                   ]
+               }),
+               ('Spaces', {
+                   'fields': [
+                       'guests',
+                       'beds',
+                       'bedrooms',
+                       'baths',
+                   ]
+               }), ('Last Details', {
+                   'fields': [
+                       'host',
+                   ]
+               })]
+  list_display = [
+      'name',
+      'country',
+      'city',
+      'price',
+      'address',
+      'guests',
+      'beds',
+      'bedrooms',
+      'baths',
+      'check_in',
+      'check_out',
+      'instant_book',
+  ]
+  list_filter = [
+      'instant_book',
+      'host__superhost',
+      'host__gender',
+      'room_type',
+      'amenities',
+      'facilities',
+      'house_rule',
+      'city',
+      'country',
+  ]
+  search_fields = ['city', '^host__username']
+  filter_horizontal = [
+      'amenities',
+      'facilities',
+      'house_rule',
+  ]
 
 
-@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+@admin.register(models.RoomType, models.Facility, models.Amenity,
+                models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
   '''Item Admin Definition '''
   pass
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
