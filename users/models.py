20dbeffa76bfdad216c23e9ec1b7aa3f949b7da8
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2022-11-04 16:48:59 +0330

    rooms model update

--- users/models.py
+++ users/models.py
@@ -37,8 +37,13 @@ class User(AbstractUser):
   birthdate = models.DateField(blank=True, null=True)
   language = models.CharField(choices=LANGUAGE_CHOICES,
                               max_length=2,
-                              blank=True, null=True)
+                              blank=True,
+                              null=True)
   currency = models.CharField(choices=CURRENCY_CHOICES,
                               max_length=3,
-                              blank=True, null=True)
+                              blank=True,
+                              null=True)
   superhost = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.username
