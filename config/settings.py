5e07bdae9316aae26924cec1ab1b1b25de434aad
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2022-11-04 17:24:51 +0330

    add model List

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ PROJECT_APPS = [
     'core.apps.CoreConfig',
     'reviews.apps.ReviewsConfig',
     'reservations.apps.ReservationsConfig',
+    'lists.apps.ListsConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
