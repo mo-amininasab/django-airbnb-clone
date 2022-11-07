6176e3df72ffc6e39933d26445e435f13e838785
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2022-11-07 15:15:54 +0330

    paginator done

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.paginator import Paginator
 from rooms import models as room_models
 
 def all_rooms(request):
-  query = room_models.Room.objects.all()
+  page = request.GET.get('page')
+  room_list = room_models.Room.objects.all()
+  paginator = Paginator(room_list, 10)
+  rooms = paginator.get_page(page)
+
   context = {
-    'rooms': query
+    'rooms': rooms
   }
   return render(request, 'rooms/home.html', context)
 
