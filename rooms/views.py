41b4bd19b036e725aa7e57b20485755c949610b2
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2022-11-07 15:26:14 +0330

    update paginator, exception handling

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from rooms import models as room_models
 
+
 def all_rooms(request):
-  page = request.GET.get('page')
+  page = request.GET.get('page', 1)
   room_list = room_models.Room.objects.all()
-  paginator = Paginator(room_list, 10)
-  rooms = paginator.get_page(page)
-
-  context = {
-    'rooms': rooms
-  }
-  return render(request, 'rooms/home.html', context)
-
-
+  paginator = Paginator(room_list, 10, orphans=5)
+  try:
+    rooms = paginator.page(page)
+    return render(request, 'rooms/home.html', {'rooms': rooms})
+  except EmptyPage:
+    return redirect('/')
