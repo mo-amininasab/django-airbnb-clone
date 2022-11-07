5e4ab18de92c8508a602a8ff543f7da15b69293f
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2022-11-07 14:51:55 +0330

    template initial

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rooms import models as room_models
 
-# Create your views here.
+def all_rooms(request):
+  query = room_models.Room.objects.all()
+  context = {
+    'rooms': query
+  }
+  return render(request, 'rooms/home.html', context)
+
+
