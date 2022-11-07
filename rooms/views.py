0cb88fef31485cf2464ad885c39d7d24f4e8a2b1
mohammad_amini_nasab <mohammad_amini_nasab@yahoo.com>
2022-11-07 18:27:41 +0330

    form done

--- rooms/views.py
+++ rooms/views.py
@@ -22,17 +22,43 @@ def search(request):
   city = str.capitalize(city)
   country = request.GET.get('country', 'KR')
   room_type = int(request.GET.get('room_type', 0))
-  room_types = room_models.RoomType.objects.all()
+
+  price = int(request.GET.get('price', 0))
+  guests = int(request.GET.get('guests', 0))
+  bedrooms = int(request.GET.get('bedrooms', 0))
+  beds = int(request.GET.get('beds', 0))
+  baths = int(request.GET.get('baths', 0))
+
+  instant = request.GET.get('instant', False)
+  super_host = request.GET.get('super_host', False)
+  print(instant, super_host)
+
+  selected_amenities = request.GET.getlist('amenities')
+  selected_facilities = request.GET.getlist('facilities')
 
   form = {
       'city': city,
       'selected_room_type': room_type,
       'selected_country': country,
+      'price': price,
+      'guests': guests,
+      'bedrooms': bedrooms,
+      'beds': beds,
+      'baths': baths,
+      'instant': instant,
+      'super_host': super_host,
+      'selected_amenities': selected_amenities,
+      'selected_facilities': selected_facilities,
   }
 
+  room_types = room_models.RoomType.objects.all()
+  amenities = room_models.Amenity.objects.all()
+  facilities = room_models.Facility.objects.all()
   choices = {
       'countries': countries,
       'room_types': room_types,
+      'amenities': amenities,
+      'facilities': facilities,
   }
 
   return render(request, 'rooms/search.html', {**form, **choices})
